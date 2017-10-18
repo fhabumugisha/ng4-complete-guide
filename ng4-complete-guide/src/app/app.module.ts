b42ead5387a0e50b7f1ed3fc76f24acfdb5d3961
HABUMUGISHA <fabrice.a.habumugisha@capgemini.com>
2017-10-18 20:07:22 +0200

    Add select recipe feature

--- ng4-complete-guide/src/app/app.module.ts
+++ ng4-complete-guide/src/app/app.module.ts
@@ -1,3 +1,7 @@
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-list/recipe-detail/recipe-detail.component';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,10 +9,11 @@ import { AppComponent } from './app.component';
 
 import { HeaderComponent} from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
+
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe-list/recipe-detail/recipe-detail.component'
+
+
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +24,7 @@ import { RecipeDetailComponent } from './recipe-list/recipe-detail/recipe-detail
     ShoppingEditComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
+    RecipesComponent,
   ],
   imports: [
     BrowserModule
--- ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {Recipe} from '../recipe-list/recipe.model';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Recipe } from '../recipe-list/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,15 +7,31 @@ import {Recipe} from '../recipe-list/recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-recipes: Recipe[] = [
+  selectedRecipe: Recipe;
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+ recipes: Recipe[] = [
   new Recipe('A test recipe', 'This is a simple test',
    'https://get.pxhere.com/photo/dish-food-recipe-fast-food-cuisine-garnish-snacks-vegetarian-food-power-supply-appetizer-tidbits-side-dish-arancini-hors-d-oeuvre-finger-food-rissole-croquette-fried-food-pakora-chicken-nugget-american-food-esfiha-kibe-vetkoek-1375826.jpg'),
-   new Recipe('A test recipe', 'This is a simple test',
+   new Recipe('A test recipe 2', 'This is a simple test 2',
    'https://get.pxhere.com/photo/dish-food-recipe-fast-food-cuisine-garnish-snacks-vegetarian-food-power-supply-appetizer-tidbits-side-dish-arancini-hors-d-oeuvre-finger-food-rissole-croquette-fried-food-pakora-chicken-nugget-american-food-esfiha-kibe-vetkoek-1375826.jpg')
 ];
-  constructor() { }
+
+
+ 
+constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeWasSelected(recipeData: Recipe) {
+    this.recipeWasSelected.emit(recipeData);
+  }
+
+  
+  onRecipeSelected(recipeData: Recipe){
+    console.log('Selected recipe : ' + recipeData.name);
+    this.recipeWasSelected.emit(recipeData);
+  }
 }
+
+
