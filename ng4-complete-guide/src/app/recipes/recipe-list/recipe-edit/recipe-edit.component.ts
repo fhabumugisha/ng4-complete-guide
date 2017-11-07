d9a542a4d293c4f2baf806c9a456de57fb79e224
HABUMUGISHA <fabrice.a.habumugisha@capgemini.com>
2017-11-07 06:59:03 +0100

    Add edit recipe navigation

--- ng4-complete-guide/src/app/recipes/recipe-list/recipe-edit/recipe-edit.component.ts
+++ ng4-complete-guide/src/app/recipes/recipe-list/recipe-edit/recipe-edit.component.ts
@@ -1,3 +1,5 @@
+import { RecipeService } from './../../recipe.service';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
-
-  constructor() { }
+id: number;
+editMode = false;
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute,
+     private router: Router) { }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+          this.id = +params['id'];
+          this.editMode =  params['id'] != null;
+          console.log(this.editMode);
+      }
+    );
   }
 
 }
